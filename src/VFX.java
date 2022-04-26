c344b222d97c1bdd7a6348ff60bbc1d49e896499
Brayckner <brayckner@gmail.com>
2022-04-26 15:54:09 -0400

    Adding some of the classes that are in the UML diagram, and this is testing the project setup.

---
+++ src/VFX.java
@@ -0,0 +1,13 @@
+public class VFX {
+
+    private Material texture;
+
+    public Material getMaterial() {
+        return this.texture;
+    }
+
+    public void setMaterial(Material texture){
+        this.texture = texture;
+    }
+    
+}
