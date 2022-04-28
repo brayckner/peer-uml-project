ac54130543a011c764c31b30941fbfe6c8c3a66a
Nicholas Fulton <57047559+nickornator9000@users.noreply.github.com>
2022-04-28 16:40:56 -0400

    Update Material.java
    I implemented the materials class with object stuff

---
+++ src/Material.java
@@ -0,0 +1,21 @@
+import java.io.File;
+
+public class Material{
+
+    private File imagefile;
+    private double offsetX;
+    private double offsetY;
+    private double rot;
+    private double scaX;
+    private double scaY;
+    public Material(){
+        File imageFile;
+        double offsetX;
+        double offsety;
+        double rot;
+        double scaX;
+        double scaY;
+
+    }
+    
+}
