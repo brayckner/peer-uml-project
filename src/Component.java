4763112a0b2ebf867d98a10266608ff4db690b80
nickornator9000 <nicholasfulton14@gmail.com>
2022-04-30 17:28:56 -0400

    Update Component Class
    Wrote code for component class, still need to add in transform method.

--- src/Component.java
+++ src/Component.java
@@ -1,4 +1,15 @@
 public class Component {
+    private String name;
+    private boolean isHidden;
+    private ComplexTransform compTransform;
+    private int componentID;
+
+    public void hide(){isHidden = true;}
+    public String getName(){return name;}
+    public void setName(String s){name =s;}
+    public int getComponentID(){return componentID;}
+    public void setComponentID(int ID){componentID=ID;}
+
 
     
     
