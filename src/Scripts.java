43b261fd0b13e91ccdb8a42d17495f2e0a8a6e2b
Conner0135 <104568160+Conner0135@users.noreply.github.com>
2022-05-01 17:42:17 -0400

    Update Scripts.java

--- src/Scripts.java
+++ src/Scripts.java
@@ -1,4 +1,21 @@
 public class Scripts {
-
+    public List<File> scripts;
+    public File getScript(int index)
+    {
+      
+    }
+    public void add(File f)
+    {
+        
+    }
+    
+    public void remove(int index)
+    {
+        
+    }
+    public List<String> translate(File f)
+    {
+ 
+    }
     
 }
