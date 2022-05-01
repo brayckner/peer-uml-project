d0221f26c41efbb94c401e76662c5f20a4284558
Conner0135 <104568160+Conner0135@users.noreply.github.com>
2022-05-01 14:28:23 -0400

    Create Timeline.java

---
+++ src/Timeline.java
@@ -0,0 +1,12 @@
+public class Timeline {
+    public List<String> animIntr;
+    public Scripts scriptList;
+    public int getActiveFrame()
+    {
+    
+    }
+    public void setActiveFrame(int i)
+    {
+     
+    }
+}
