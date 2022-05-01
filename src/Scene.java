12df7914e14c8ab02e829e887ff31be70f9b6710
Conner0135 <104568160+Conner0135@users.noreply.github.com>
2022-05-01 14:12:11 -0400

    Update Scene.java

--- src/Scene.java
+++ src/Scene.java
@@ -1,5 +1,25 @@
-public class Scene {
-
-
-
+public class Scene implements XDObject {
+  int sceneID;
+  String name;
+  List<XDObject> objectList;
+  public int getID()
+  {
+   return sceneID;
+  }
+  public void setID(int i)
+  {
+   sceneID = i;
+  }
+  public String getName()
+  {
+   return name;
+  }
+  public void setName(String s)
+  {
+   name = s;
+  }
+  public List<XDObject> objects()
+  {
+    return objectList;
+  }
 }
